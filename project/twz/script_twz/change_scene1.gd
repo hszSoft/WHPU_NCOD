772907d9e056c06a2ea90c68d8881b6b0ce26d5c
theJayreal <zhouwenjun472@gmail.com>
2024-04-08 12:13:42 +0800

    导入了我的粗稿

---
+++ project/twz/script_twz/change_scene1.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+
+
+var entered=false
+ 	
+func _on_body_entered(body:PhysicsBody2D):
+	entered = true
+	pass # Replace with function body.
+
+
+func _on_body_exited(body):
+	entered =false
+	pass # Replace with function body.
+func _process(delta):
+	if entered == true:
+		if Input.is_action_just_pressed("ui_accept"):
+			get_tree().change_scene_to_file("res://twz/scene/Scene2.tscn")
